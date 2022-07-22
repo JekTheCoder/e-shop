eff9514e421019b68f3f32d8a7dc3c12492c6c16
Jeshua <hinostrozajeshua@gmail.com>
2022-07-22 14:11:56 -0500

    base chip controller created

---
+++ src/app/modules/carousel/components/base-chip-controller/base-chip-controller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-chip-controller',
+  templateUrl: './base-chip-controller.component.html',
+  styleUrls: ['./base-chip-controller.component.scss']
+})
+export class BaseChipControllerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
