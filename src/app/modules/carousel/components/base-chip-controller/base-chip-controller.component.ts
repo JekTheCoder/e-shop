6007b4c3cffadcee9d6a1b0b96fb5dc566263742
Jeshua <hinostrozajeshua@gmail.com>
2022-07-22 14:17:16 -0500

    base chip controller inputs and outputs created

--- src/app/modules/carousel/components/base-chip-controller/base-chip-controller.component.ts
+++ src/app/modules/carousel/components/base-chip-controller/base-chip-controller.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'app-base-chip-controller',
-  templateUrl: './base-chip-controller.component.html',
-  styleUrls: ['./base-chip-controller.component.scss']
+  template: ''
 })
-export class BaseChipControllerComponent implements OnInit {
+export abstract class BaseChipControllerComponent implements OnInit {
+
+  @Input() chip = 0;
+  @Output() chipChange = new EventEmitter<number>();
+
+  @Input() chipTemplate!: TemplateRef<any>;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (!this.chipTemplate) throw new Error('chip template is required');
   }
 
 }
