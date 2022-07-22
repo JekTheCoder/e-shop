aa21f6fa36a7b220dbeb6ac971283d57e3e3e108
Jeshua <hinostrozajeshua@gmail.com>
2022-07-22 16:57:49 -0500

    some styles improved and a uncompleted star rate component created

--- src/app/modules/item-shop/item-shop.module.ts
+++ src/app/modules/item-shop/item-shop.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ItemShopRoutingModule } from './item-shop-routing.module';
 import { ItemShopComponent } from './components/item-shop/item-shop.component';
+import { NgLetDirective } from '@common/directives/ng-let.directive';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 
 @NgModule({
   declarations: [
-    ItemShopComponent
+    ItemShopComponent,
+    StarRatingComponent
   ],
   imports: [
     CommonModule,
-    ItemShopRoutingModule
+    ItemShopRoutingModule,
+    NgLetDirective
   ]
 })
 export class ItemShopModule { }
