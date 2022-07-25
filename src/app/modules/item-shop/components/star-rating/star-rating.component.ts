73ddb1a1675ca7157a68fc74cab5d3c46426e105
Jeshua <hinostrozajeshua@gmail.com>
2022-07-25 18:12:47 -0500

    star rating component completed

--- src/app/modules/item-shop/components/star-rating/star-rating.component.ts
+++ src/app/modules/item-shop/components/star-rating/star-rating.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ * @property {number} points - Scale between 0 and 5 stars. Admits decimals
+ */
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.scss']
 })
-export class StarRatingComponent implements OnInit {
+export class StarRatingComponent {
 
-  @Input() points: number = 0;
-  @Input() count: number = 0;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input('points') set pointsSetter(points: number) {
+    if (points < 0 || points > 5) 
+      throw new Error('points should be between 0 and 5');
+    this.points = points; 
   }
+  protected points!: number;
+
+  @Input() count: number = 0;
 
 }
