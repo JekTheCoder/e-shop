4da62048381925c844d43fef73ed42d2b21ce706
Jeshua <hinostrozajeshua@gmail.com>
2022-07-27 21:00:06 -0500

    double way filters data binding with url query params finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent {
   constructor(protected router: Router) {}
 
   redirectToSearch(search: string) {
-    this.router.navigate(['search'], { queryParams: { value: search } });
+    this.router.navigate(['search'], { queryParams: { title: search } });
   }
 }
--- src/app/modules/products-searcher/components/products-searcher/products-searcher.component.ts
+++ src/app/modules/products-searcher/components/products-searcher/products-searcher.component.ts
@@ -1,26 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FakeStoreService, Product } from '@common/services/fake-store.service';
+import { map, mergeMap, Observable, ReplaySubject, Subject, takeUntil, tap } from 'rxjs';
+
+interface Filters {
+  title?: string | null,
+  categories?: string[]
+}
 
 @Component({
   selector: 'app-products-searcher',
   templateUrl: './products-searcher.component.html',
   styleUrls: ['./products-searcher.component.scss']
 })
-export class ProductsSearcherComponent implements OnInit {
+export class ProductsSearcherComponent implements OnInit, OnDestroy {
 
-  constructor(protected router: Router, protected route: ActivatedRoute) { }
+  protected productsFiltered$?: Observable<Product[]>;
+  protected filters$ = new ReplaySubject<Filters>(1);
+
+  protected unsuscriber$ = new Subject<void>();
+
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected store: FakeStoreService
+  ) { }
 
   ngOnInit(): void {
+    this.filters$.next({ categories: [] });
+
+    this.route.queryParams
+      .pipe(takeUntil(this.unsuscriber$))
+      .subscribe(queryParams => this.filters$.next(queryParams));
+
+    this.productsFiltered$ =
+      this.store.getSomeProducts()
+        .pipe(mergeMap(products => this.filters$.pipe(map(filters => this.filterProducts(products, filters)))));
   }
 
-  test(value: string) {
-    console.log('recieved', value)
+  ngOnDestroy(): void {
+    this.unsuscriber$.next();
+    this.unsuscriber$.complete();
+  }
+
+  protected filterProducts(products: Product[], filters: Filters) {
+    return products.filter(product => (
+      product.title.toLowerCase().includes((filters.title || '').toLowerCase()) &&
+      ((!filters.categories || filters.categories!.length === 0) || Boolean(filters.categories!.find(category => category.toLowerCase() === product.category)))
+    )
+    )
   }
 
   search(value: string) {
     this.router.navigate([], {
       relativeTo: this.route,
-      queryParams: { value },
+      queryParams: { title: value },
       queryParamsHandling: 'merge'
     });
   }
