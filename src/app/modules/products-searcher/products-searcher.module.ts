722868297a3a45722a47b9907c7154e7b5e22657
Jeshua <hinostrozajeshua@gmail.com>
2022-07-29 14:18:32 -0500

    styles improved for other filters component

--- src/app/modules/products-searcher/products-searcher.module.ts
+++ src/app/modules/products-searcher/products-searcher.module.ts
@@ -6,15 +6,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { ProductsSearcherRoutingModule } from './products-searcher-routing.module';
+
+import { RemoveFilterPipe } from './pipes/remove-filter.pipe';
 
 import { ProductsSearcherComponent } from './components/products-searcher/products-searcher.component';
 import { OtherFiltersComponent } from './components/other-filters/other-filters.component';
 import { ProductLabelComponent } from './components/product-label/product-label.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { StarRatingComponent } from '@common/components/star-rating/star-rating.component';
-import { RemoveFilterPipe } from './pipes/remove-filter.pipe';
 
 
 @NgModule({
@@ -33,6 +35,7 @@ import { RemoveFilterPipe } from './pipes/remove-filter.pipe';
     MatButtonModule,
     MatSelectModule,
     MatFormFieldModule,
+    MatInputModule,
     StarRatingComponent
   ]
 })
